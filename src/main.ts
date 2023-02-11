3fc0ea33ba473cc27d7049479aad7c21d07d8148
David Garcia <Davidgarciama@outlook.com>
2023-02-11 11:47:05 -0600

    Edge cases

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-const race = (v1: number, v2: number, g: number): number[] => {
+const race = (v1: number, v2: number, g: number): number[] | null => {
+    if (v1 > v2) {
+        return null
+    }
     let minutes: number = 0;
     let seconds: number = 0;
     let time: number[] = [];
@@ -8,18 +11,22 @@ const race = (v1: number, v2: number, g: number): number[] => {
         return time;
     }
     const transformTime = (hoursObtained: number): number[] => {
-        minutes = (hoursObtained % 1) * 60;
-        seconds = (minutes % 1) * 60;
-
-        time.push(Math.floor(hoursObtained), Math.floor(minutes), Math.floor(seconds));
+        minutes = (hoursObtained % 1) * 60
+        seconds = (minutes % 1) * 60 
+        if(Math.round(seconds)==60){
+            minutes+=1
+            seconds=0
+        }
+        time.push(Math.floor(hoursObtained), Math.floor(minutes), Math.round(seconds))
         return time;
     }
     return getTime();
 }
 
 
-console.log(race(720, 850, 70))// [0, 32, 18]);
-console.log(race(80, 91, 37))// [3, 21, 49]);
-console.log(race(80, 100, 40))// [2, 0, 0]);
-console.log(race(720, 850, 37))// [0, 17, 4]);
-
+// console.log(race(720, 850, 70))// [0, 32, 18]);
+// console.log(race(80, 91, 37))// [3, 21, 49]);
+// console.log(race(80, 100, 40))// [2, 0, 0]);
+// console.log(race(720, 850, 37))// [0, 17, 5]);
+// console.log(race(820, 850, 550))//  [ 18, 20, 00 ]);
+// console.log(race(622, 847, 109))//  [ +0, 29, 4 ]);
