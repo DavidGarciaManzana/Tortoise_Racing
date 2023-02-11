7adcca3f1173b9a1e02ed6fa76b867d0727f8d27
David Garcia <Davidgarciama@outlook.com>
2023-02-11 10:19:31 -0600

    Image with the math calculations to get the formula added

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,14 @@
 // Think of calculation by hand using only integers (in your code use or simulate integer division)
 // or Google: "convert decimal time to hours minutes seconds"
 
-export const race = (v1:number, v2:number, g:number) => {
-    // your code
-  }
+
+const race = (v1: number, v2: number, g: number) => {
+
+}
+
+
+console.log(720, 850, 70)// [0, 32, 18]);
+console.log(80, 91, 37)// [3, 21, 49]);
+console.log(80, 100, 40)// [2, 0, 0]);
+console.log(720, 850, 37)// [0, 17, 4]);
+
